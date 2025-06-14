62679a1be60d4b3d3b2a1ef399bc0748aec81e1d
ricard0g <ricardoguzman444@gmail.com>
2025-06-14 18:46:48 +0200

    Feat:
    - Home page ready for desktop and in dark mode

---
+++ src/assets/app.ts
@@ -0,0 +1,34 @@
+// Toggle Theme
+const themeController = document.querySelector(
+	".theme-controller"
+) as HTMLInputElement;
+const root = document.querySelector("html") as HTMLHtmlElement;
+
+const toggleTheme = () => {
+	if (themeController.checked) {
+		root.classList.add("dark");
+		localStorage.setItem("theme", "dark");
+	} else {
+		root.classList.remove("dark");
+		localStorage.setItem("theme", "light");
+	}
+};
+
+themeController.addEventListener("change", toggleTheme);
+
+if (localStorage.getItem("theme") === "dark") {
+	root.classList.add("dark");
+	themeController.checked = true;
+} else {
+	root.classList.remove("dark");
+	themeController.checked = false;
+}
+
+// Rotate the Square on Hero Page
+
+const square = document.querySelector('.square') as HTMLDivElement;
+
+document.addEventListener('scroll', () => {
+    const scrollY: number = window.scrollY;
+    square.style.transform = `rotate(${scrollY / 10}deg)`;
+})
