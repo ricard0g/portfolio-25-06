eeb94fbdb34327344452c08f7b9c26f6163fd232
ricard0g <ricardoguzman444@gmail.com>
2025-06-18 22:23:29 +0200

    feat:
    - Add Recent Posts section
    - Create each post for Recent posts
    - Start configuring Layout for actual posts

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,4 +2,20 @@ import { defineCollection, z } from "astro:content";
 
 import { glob } from "astro/loaders";
 
-const blogCollection = defineCollection({});
+const blogCollection = defineCollection({
+	loader: glob({
+		pattern: "**/*.md",
+		base: "./src/content/posts",
+	}),
+	schema: z.object({
+		title: z.string(),
+		pubDate: z.string(),
+		updateDate: z.string().optional(),
+		tags: z.array(z.string()),
+		author: z.string(),
+		image: z.string().optional(),
+		description: z.string(),
+	}),
+});
+
+export const collections = { blogCollection };
---
+++ src/content/posts/post-1.md
@@ -0,0 +1,9 @@
+---
+title: "The First Post"
+pubDate: "2025, June 18th"
+tags: ["first", "post"]
+author: "Ricardo Guzman"
+description: "My first post ever in Markdown"
+---
+
+This is the first post I've ever written
