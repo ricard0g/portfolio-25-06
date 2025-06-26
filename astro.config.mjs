1ad62fba4b82e6a41682607d08e3fc98f6d3b5be
ricard0g <ricardoguzman444@gmail.com>
2025-06-26 20:58:07 +0200

    Feat:
    - Setup sitemap and Robots.txt files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,19 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
 	markdown: {
 		shikiConfig: {
-            theme: "gruvbox-dark-soft",
+			theme: "gruvbox-dark-soft",
 		},
 		syntaxHighlight: "shiki",
 	},
 	vite: {
 		plugins: [tailwindcss()],
 	},
+	site: "https://ricardoguzman.dev",
+	integrations: [sitemap()],
 });
