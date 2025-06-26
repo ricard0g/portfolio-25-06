bf8695a6659d5fccb357a04b064808c542af9b0c
ricard0g <ricardoguzman444@gmail.com>
2025-06-26 16:48:28 +0200

    Fix:
    - Try to dynamically change color of code blocks but failed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-    }
+	markdown: {
+		shikiConfig: {
+            theme: "gruvbox-dark-soft",
+		},
+		syntaxHighlight: "shiki",
+	},
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
